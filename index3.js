f47c1c805acb6e1f8550f90c8bba9d260e64fff8
Janhavi Nitin Sonawane <71979740+janhavisonawane25@users.noreply.github.com>
2024-10-07 19:27:54 +0530

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,23 @@
+  
+        //  1. ways to print in javascript
+        //console.log("hello world");
+       // alert("me");
+       // document.write("hello there its  me");
+
+        //  2. javascript colsole API
+      console.log("hello world", 49+5, "another log");
+      console.warn("this is warning");
+      console.error("this is an error");
+
+      // 3. js variables 
+      //-> container to store data values
+      var num1 =34
+      var num2 =56;
+      console.log(num1+num2);
+
+      // opertors in javascript
+      1  Arithmetic opertor
+      2  Assignment opertor
+      3 Boolean opertor
+      4 logical opertor
+      5 bitwise opertor
